ef1129e22e929a30f26896a23bd17057803b0514
t88”git config --global user.email “1353435722@qq.com <1353435722@qq.com>
2024-10-02 19:03:59 +0800

    neue Web-app

--- src/web_app.py
+++ src/web_app.py
@@ -5,23 +5,21 @@ import os
 import plotly.graph_objects as go
 import plotly.io as pio
 from sklearn.linear_model import LinearRegression
-import matplotlib.pyplot as plt
+
 app = Flask(__name__)
 UPLOAD_FOLDER = 'uploads'
-REFERENZINTERVALL_FILE = 'referenzintervall.csv'
+REFERENZINTERVALL_FILE = 'cleaned_referenzintervall.csv'
 
 
 def read_csv_file(file_path):
     sep_options = [',', ';', '\t']
     encoding_options = ['utf-8', 'latin1']
-
     df = None
 
     for encoding in encoding_options:
         for sep in sep_options:
             try:
                 df = pd.read_csv(file_path, sep=sep, encoding=encoding)
-
                 return df
             except (pd.errors.ParserError, UnicodeDecodeError):
                 continue
@@ -41,20 +39,14 @@ def get_column_range(df, column):
 def leading_digit_analysis(data):
     MIN_NONZERO_VALUE = 1e-10
     data = data.dropna().astype(str)
-    # data = data[data.str.replace('.', '').str.isdigit()]
-    # data = data[data.str.replace(',', '').str.isdigit()]
-    # leading_digits = data.str[0].str[0].astype(int)
-
-
-    # leading_digits = data.str.split('.').str[0].str[0].astype(int)
-    data = data[data.str.replace(',', '').str.isdigit()]
-    leading_digits = data.str.split(',').str[0].str[0].astype(int)
-    leading_digits = leading_digits[leading_digits != 0]  # Filter out cases where the first digit is 0
+    data = data[data.str.replace('.', '').str.isdigit()]
+    leading_digits = data.str.split('.').str[0].str[0].astype(int)
+    leading_digits = leading_digits[leading_digits != 0]  # 过滤掉首位数字为0的情况
     leading_digit_counts = leading_digits.value_counts().sort_index()
     total_count = leading_digits.count()
     leading_digit_freq = leading_digit_counts / total_count
 
-    # Add minimum value logic
+    # 添加最小值逻辑
     for i in range(1, 10):
         if i not in leading_digit_freq:
             leading_digit_freq.loc[i] = MIN_NONZERO_VALUE
@@ -150,7 +142,7 @@ def index():
 
     context['selected_file'] = selected_file
 
-    # Read the referenzintervall.csv file
+    # 读取referenzintervall.csv文件
     referenz_df = read_csv_file(REFERENZINTERVALL_FILE)
     if referenz_df is not None:
         referenz_df = referenz_df[referenz_df['ZEITRAUM'] == 'Jahr']
@@ -187,15 +179,12 @@ def analyze():
 
         selected_column = df[column]
 
-        # Clean up non-numeric values and convert to floating point numbers
-        # selected_column = pd.to_numeric(selected_column.str.replace(',', '.'), errors='coerce').dropna()
+        # 清洗掉非数值型的值并转换为浮点数
+        selected_column = pd.to_numeric(selected_column.str.replace(',', '.'), errors='coerce').dropna()
 
         digit_freq = leading_digit_analysis(selected_column)
-        # benford_freq = benford_distribution() * digit_freq.sum()  # 调整期望频率以匹配观察频率的总和
-        benford_freq = benford_distribution()
+        benford_freq = benford_distribution() * digit_freq.sum()  # 调整期望频率以匹配观察频率的总和
         total_count = len(selected_column.dropna())
-        # print(total_count)
-
 
         fig = go.Figure()
         fig.add_trace(go.Bar(x=digit_freq.index, y=digit_freq.values, name="Actual Data"))
@@ -244,17 +233,15 @@ def stat_tests():
 
         if 'chi_square_test' in analysis_methods:
             chi2_stat = chi_square_test(digit_freq, benford_freq, total_count)
-            print(digit_freq)
-            print(benford_freq)
             results['chi_square_test'] = {'chi2_stat': chi2_stat}
             fig.add_trace(go.Scatter(x=np.arange(1, 10), y=benford_freq, mode='lines', name="Benford's Law"))
             fig.add_trace(go.Scatter(x=np.arange(1, 10), y=digit_freq, mode='lines', name="Actual Data"))
             if chi2_stat > 16.9:
                 results['chi_square_test'][
-                    'result'] = "With 8 degrees of freedom, the Chi-Square critical value at a 5% significance level is 15.51. Chi-Square value is greater than 15.51. The data does not conform to Benford's Law."
+                    'result'] = "Chi-Square value is greater than 16.9. The data does not conform to Benford's Law."
             else:
                 results['chi_square_test'][
-                    'result'] = "With 8 degrees of freedom, the Chi-Square critical value at a 5% significance level is 15.51. Chi-Square value is less than or equal to 15.51. The data conforms to Benford's Law."
+                    'result'] = "Chi-Square value is less than or equal to 16.9. The data conforms to Benford's Law."
 
         if 'z_test' in analysis_methods:
             z_scores, significant_digits = z_test(digit_freq, benford_freq, total_count)
@@ -262,9 +249,9 @@ def stat_tests():
             fig.add_trace(go.Scatter(x=np.arange(1, 10), y=benford_freq, mode='lines', name="Benford's Law"))
             fig.add_trace(go.Scatter(x=np.arange(1, 10), y=digit_freq, mode='lines', name="Actual Data"))
             if significant_digits:
-                results['z_test']['result'] = f"With 8 degrees of freedom, the z_score critical value at a 5% significance level is 1.96.Digits {significant_digits} do not conform to Benford's Law."
+                results['z_test']['result'] = f"Digits {significant_digits} do not conform to Benford's Law."
             else:
-                results['z_test']['result'] = "With 8 degrees of freedom, the z_score critical value at a 5% significance level is 1.96.All digits conform to Benford's Law."
+                results['z_test']['result'] = "All digits conform to Benford's Law."
 
         if 'measure_of_distance' in analysis_methods:
             distance = measure_of_distance(digit_freq, benford_freq)
@@ -276,30 +263,15 @@ def stat_tests():
             slope, intercept = regression_measure(digit_freq)
             results['regression_measure'] = {'slope': slope, 'intercept': intercept}
             benford_proportions = np.log10(1 + 1 / np.arange(1, 10))
-            observed_proportions = digit_freq
+            observed_proportions = np.log10(digit_freq)
             fig = go.Figure()
-
-
-            actual_data_color = 'blue'
-            regression_line_color = 'blue'
-
-            # Add Actual Data and Regression Line
-            fig.add_trace(go.Scatter(x=benford_proportions, y=observed_proportions, mode='markers', name="Actual Data",
-                                     marker=dict(color=actual_data_color)))
+            fig.add_trace(go.Scatter(x=benford_proportions, y=observed_proportions, mode='markers', name="Actual Data"))
             fig.add_trace(go.Scatter(x=benford_proportions, y=slope * benford_proportions + intercept, mode='lines',
-                                     name='Regression Line', line=dict(color=regression_line_color)))
-
-            # Add ideal line segments
-            fig.add_trace(go.Scatter(x=benford_proportions, y=benford_proportions, mode='lines', name='Ideal Line',
-                                     line=dict(color='red')))
-
-            fig.update_layout(
-                title='Saville Regression Measure',
-                xaxis_title='Theoretical Benford',
-                yaxis_title='Actually obseved',
-
-            )
-
+                                     name='Regression Line'))
+            fig.update_layout(title='Saville Regression Measure',
+                              xaxis_title='Log10(Expected)',
+                              yaxis_title='Log10(Observed)',
+                              height=300)
             results['regression_plot_html'] = pio.to_html(fig, full_html=False)
 
         if fig.data:
@@ -348,41 +320,35 @@ def interval_analyze():
 
         df = df.dropna(subset=[column])
 
-        # Find the corresponding interval based on the age range entered by the user
+        # 根据用户输入的年龄范围找到对应的 interval
         interval_filtered = referenz_df[(referenz_df['CODE'] == code) & (referenz_df['ALTERVON'] <= age_from) & (
                     referenz_df['ALTERBIS'] >= age_to) & ((referenz_df['SEX'].str.lower() == sex_value) | (
                     referenz_df['SEX'].str.lower() == 'al'))]
         if interval_filtered.empty:
             return jsonify({'error': "No matching interval found for the given age range and sex."})
 
-        # print(age_from)
-        # print(age_to)
-
         interval_age_from = interval_filtered['ALTERVON'].min()
         interval_age_to = interval_filtered['ALTERBIS'].max()
 
-        # print(interval_age_from)
-        # print(interval_age_to)
-
-        # Filtering age range and gender
+        # 过滤年龄范围和性别
         df_age_filtered = df[(df['ALTER'] >= interval_age_from) & (df['ALTER'] <= interval_age_to) & (
                     df['SEX'].str.lower() == sex_value)]
 
-        # Cleaned and converted to floating point
+        # 清洗并转换列为浮点数
         df_age_filtered[column] = pd.to_numeric(df_age_filtered[column].str.replace(',', '.'), errors='coerce').dropna()
 
-        # Get data for normal range
+        # 获取 normal range 的数据
         normal_from = interval_filtered['NORMVON'].values[0]
         normal_to = interval_filtered['NORMBIS'].values[0]
 
         normal_from = float(normal_from.replace(',', '.')) if ',' in str(normal_from) else float(normal_from)
         normal_to = float(normal_to.replace(',', '.')) if ',' in str(normal_to) else float(normal_to)
 
-        # Sliced data
+        # 切片数据
         df_normal_filtered = df_age_filtered[
             (df_age_filtered[column] >= normal_from) & (df_age_filtered[column] <= normal_to)]
 
-        # Analyze all data with an age in the interval range
+        # 分析年龄在 interval 范围内的所有数据
         digit_freq_all = leading_digit_analysis(df_age_filtered[column])
         benford_freq = benford_distribution() * digit_freq_all.sum()
         total_count_all = len(df_age_filtered[column].dropna())
@@ -398,7 +364,7 @@ def interval_analyze():
 
         plot_html_all = pio.to_html(fig_all, full_html=False)
 
-        # Analyze normal range data
+        # 分析 normal range 数据
         digit_freq_normal = leading_digit_analysis(df_normal_filtered[column])
         total_count_normal = len(df_normal_filtered[column].dropna())
 
@@ -431,6 +397,7 @@ if __name__ == '__main__':
         os.makedirs(UPLOAD_FOLDER)
     if not os.path.exists('static'):
         os.makedirs('static')
-    app.run(debug=True, port=5001)
+    app.run(debug=True, port=5009)
 
 
+#
